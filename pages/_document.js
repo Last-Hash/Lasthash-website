e3f1ad79069366579b3f28a1d60ae449b89d19c0
Shiv Singh <shivsingh7150@hotmail.com>
2024-07-04 15:35:52 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital@0;1&family=Rubik:wght@400;500;700;900&display=swap"
+    rel="stylesheet" />
+            </Head>
             <body>
             <Main />
             <NextScript />
