9c50c63022482e8d427fc3f73126a711d9245c4f
Shiv Singh <shivsingh7150@hotmail.com>
2024-07-04 16:44:48 +0530

    app

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,19 @@ import 'public/css/main.css'
 
 
 import Head from 'next/head'
- 
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="viewport-fit=cover" />
+                <meta charSet="utf-8" />
+                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital@0;1&family=Rubik:wght@400;500;700;900&display=swap"
+                    rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
- 
+
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,7 @@ export default function Document() {
     return (
         <Html className="no-js" lang="en">
             <Head>
-            <meta charSet="utf-8"/>
-            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital@0;1&family=Rubik:wght@400;500;700;900&display=swap"
-    rel="stylesheet" />
+            
             </Head>
             <body>
             <Main />
