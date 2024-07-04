3cfcfb6ed24429fc7717eddddd4a14b0dab381b1
Shiv Singh <shivsingh7150@hotmail.com>
2024-07-04 14:25:21 +0530

    js adding

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+import 'public/css/normalize.css'
+import 'public/css/bootstrap.min.css'
+import 'public/css/fontawesome.css'
+import 'public/css/magnific-popup.css'
+import 'public/css/owl.carousel.min.css'
+import 'public/css/owl.theme.default.min.css'
+import 'public/css/main.css'
+
 
 function Application({ Component, pageProps }) {
     return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+import Head from 'next/head'
+
+import Meta from '@components/Meta'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+
+
+const Home = () => {
+    return (
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
+
+
+export default Home
